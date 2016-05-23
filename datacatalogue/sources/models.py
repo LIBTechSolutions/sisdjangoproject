b8fd1ff96e7dcaddcd3cdf18b5e9822c5e4eaa70
nkhumphreys <nkhumphreys@gmail.com>
2016-05-23 13:51:59 +0000

    Add partial datasource model
    This commit contains a partial model for the datasource

--- datacatalogue/sources/models.py
+++ datacatalogue/sources/models.py
@@ -5,6 +5,20 @@ from django.utils.translation import ugettext as _
 
 # Create your models here.
 
+ACCESS_METHOD_CHOICE = (
+    (u"HTTP GET", u"HTTP GET"),
+    (u"HTTP POST", u"HTTP POST"),
+    (u"FTP", u"FTP"),
+    (u"SFTP", u"SFTP"),
+    (u"GOOGLE DRIVE", u"GOOGLE DRIVE")
+    (u"S3", u"S3"),
+    (u"EMAIL", u"EMAIL"),
+)
+
+# ToDo
+COMPRESSION_CHOICES = (
+)
+
 
 class Source(models.Model):
 
@@ -21,6 +35,38 @@ class Source(models.Model):
         on_delete=models.SET_NULL,
         related_name='sources',
         related_query_name='source',
+        null=False,
+        blank=False
+    )
+
+    access_method = models.CharField(
+        _('Access Method'),
+        max_length=30,
+        choices=ACCESS_METHOD_CHOICE,
+        default=u"HTTP GET",
+        null=False,
+        blank=False
+    )
+
+    availability = models.BoolField(
+        _('Availability'),
+        default=True,
+        null=True,
+        blank=True
+    )
+
+    # ToDo
+    compression = models.CharField(
+        _('Compression'),
+        max_length=30,
+        choices=COMPRESSION_CHOICES,
+        default=u'',
+        null=False,
+        blank=False
+    )
+
+    description = models.TextField(
+        _('Description'),
         null=False,
         blank=False
     )
