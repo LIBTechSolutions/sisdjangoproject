3e2a61035379496f7edda810c049508d4c432879
Israel Z. Kollie <zikollie@gmail.com>
2016-05-25 09:33:55 -0700

    Added LDAP and Contact models to the users app plus rest_framework

--- datacatalogue/users/apps.py
+++ datacatalogue/users/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class TenantsConfig(AppConfig):
-    name = 'tenants'
+class UsersConfig(AppConfig):
+    name = 'users'
